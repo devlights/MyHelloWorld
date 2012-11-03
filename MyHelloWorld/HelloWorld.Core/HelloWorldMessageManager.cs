0d09f345dacc77482fc42016bf691b2c08ba1615
devlights <gsf.zero1@gmail.com>
2012-11-03 17:33:30 +0900

    add HelloWorld.Core project.

---
+++ MyHelloWorld/HelloWorld.Core/HelloWorldMessageManager.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.Core
+{
+  public class HelloWorldMessageManager : IMessageManager
+  {
+    private readonly string _name;
+
+    public HelloWorldMessageManager(string name)
+    {
+      _name = name;
+    }
+
+    public string GetMessage()
+    {
+      return string.Format("Hello World {0}!", _name); 
+    }
+  }
+}
