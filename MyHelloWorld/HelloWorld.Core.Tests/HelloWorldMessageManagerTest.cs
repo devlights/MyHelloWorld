b71d788f18ea495c4ae209585baa1c0afc5d678d
devlights <gsf.zero1@gmail.com>
2012-11-03 17:37:21 +0900

    add HelloWorld.Core.Tests project.

---
+++ MyHelloWorld/HelloWorld.Core.Tests/HelloWorldMessageManagerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorld.Core.Tests
+{
+  [TestClass]
+  public class HelloWorldMessageManagerTest
+  {
+    [TestMethod]
+    public void Test_GetMessage_Success()
+    {
+      // Arrange.
+      var manager = new HelloWorldMessageManager("test");
+
+      // Act.
+      var result  = manager.GetMessage();
+
+      // Assert.
+      var expected = "Hello World test!";
+      Assert.AreEqual<string>(expected, result);
+    }
+  }
+}
